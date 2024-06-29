6330a76e12771746691e750f1773a4f243943539
GuoChenxu123 <www.1925620389@foxmail.com>
2024-06-29 23:40:10 +0800

    first commit

---
+++ scenes/player/player.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+}
